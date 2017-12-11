e76af4124e5a742e99b5503c2586a4d24fc357d5
bilalghouri <bilal@linksnappy.com>
2017-12-11 07:45:18 +0500

    chatbot update

--- botman/routes/web.php
+++ botman/routes/web.php
@@ -1,19 +1,6 @@
-
-
-
-    return view('welcome');
-*/
-/*
 <?php
 Route::get('/', function () {
+return view('welcome');
+});
 Route::get('/botman/tinker', 'BotManController@tinker');
 Route::match(['get', 'post'], '/hook', 'BotManController@handle');
-|
-|
-| contains the "web" middleware group. Now create something great!
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| Web Routes
-|--------------------------------------------------------------------------
-|--------------------------------------------------------------------------
-});
